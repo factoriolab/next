fb266d1537dbbc9b6ff5f0cc0e8a721701dddf6f
dcbroad3 <dcbroad3@gmail.com>
2025-10-17 20:12:57 +0000

    Deploying to main from @ factoriolab/factoriolab@09da6d04786278179d234a2cccfc989ed74eacaa 🚀

--- chunk-3EPAYQSI.js
+++
@@ -1,1 +0,0 @@
-import{Lb as n,Mb as o,gc as i,hb as a}from"./chunk-BGREWA57.js";import"./chunk-HZ6M6AS2.js";var e=class e{};e.\u0275fac=function(t){return new(t||e)},e.\u0275cmp=a({type:e,selectors:[["lab-data"]],decls:2,vars:0,template:function(t,m){t&1&&(n(0,"p"),i(1,"data works!"),o())},encapsulation:2,changeDetection:0});var s=e;export{s as Data};
--- chunk-TH3BFGP2.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-HZ6M6AS2.js";var o=[{path:"list",loadComponent:()=>import("./chunk-4BPZXJ2I.js").then(t=>t.Steps)},{path:"flow",loadComponent:()=>import("./chunk-4FS2XAZB.js").then(t=>t.Flow)},{path:"data",loadComponent:()=>import("./chunk-3EPAYQSI.js").then(t=>t.Data)}];export{o as routes};
