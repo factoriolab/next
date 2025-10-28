fd984718fa8a32d0a15ec2e26aa27b266688ace6
dcbroad3 <dcbroad3@gmail.com>
2025-10-28 03:55:27 +0000

    Deploying to main from @ factoriolab/factoriolab@7bb52724272f68ebb353a7fe71888a04b587a74a 🚀

--- chunk-F5VGECJY.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-HZ6M6AS2.js";var o=[{path:"list",loadComponent:()=>import("./chunk-2JMFUF5S.js").then(t=>t.Steps)},{path:"flow",loadComponent:()=>import("./chunk-ITELJHGH.js").then(t=>t.Flow)},{path:"data",loadComponent:()=>import("./chunk-4ACEZ7WT.js").then(t=>t.Data)}];export{o as routes};
